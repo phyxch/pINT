3ab0f9f57cab95c1be02753b272a3b2b54a831c9
phyxch <phyxch@gmail.com>
2024-03-20 11:32:59 -0400

    update RunAction

--- src/pINTRunAction.cc
+++ src/pINTRunAction.cc
@@ -10,7 +10,9 @@
 //    Removed g4root.hh and implemented G4AnalysisManager. Added ".root" extension to the ourput filename.
 //
 // Updated on March 19, 2024: hexc
-//    Cleaning up code and adding more comments. Also, added time and date info to the output filename.
+//    Cleaning up code and adding more comments.
+//    Moved the histogram/ntuple creation to the constructor
+//    Also, added time and date info to the output filename.
 //
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -32,22 +34,8 @@ using G4AnalysisManager = G4GenericAnalysisManager;
 
 pINTRunAction::pINTRunAction()
   : G4UserRunAction()
-{;}
-
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-pINTRunAction::~pINTRunAction()
-{;}
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-void pINTRunAction::BeginOfRunAction(const G4Run* aRun)
-{ 
-  G4cout << "### Run " << aRun->GetRunID() << " start." << G4endl;
-
+{
   // Get the analysis manager
-  // Create analysis manager
   auto analysisManager = G4AnalysisManager::Instance();
   G4cout << "Using " << analysisManager->GetType() << G4endl;
 
@@ -60,22 +48,6 @@ void pINTRunAction::BeginOfRunAction(const G4Run* aRun)
   analysisManager->SetVerboseLevel(1);
   analysisManager->SetNtupleMerging(true);
   // Note: merging ntuples is available only with Root output
-  
-  // Get time and date to creating a unique filename
-  time_t rawtime;
-  struct tm * timeinfo;
-  char buffer[80];
-  
-  time (&rawtime);
-  timeinfo = localtime(&rawtime);
-  
-  strftime(buffer,80,"-%d-%m-%Y-%H-%M-%S",timeinfo);
-  G4String DateTime(buffer);
-  
-  // Open an output file
-  G4String fileName = "pINT_output" + DateTime + ".root";  
-  //  G4String fileName = "pINT_output.root";
-  analysisManager->OpenFile(fileName);
 
   analysisManager->CreateH1("Eloss","Edep in shell", 100, 0., 100*MeV);
   //analysisManager->CreateH1("Egap","Edep in gap", 100, 0., 100*MeV);
@@ -94,7 +66,40 @@ void pINTRunAction::BeginOfRunAction(const G4Run* aRun)
   fNtColID[8] = analysisManager->CreateNtupleDColumn(1, "diffKin");
 
   analysisManager->FinishNtuple(1);
+
+}
+
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+pINTRunAction::~pINTRunAction()
+{;}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void pINTRunAction::BeginOfRunAction(const G4Run* aRun)
+{ 
+  G4cout << "### Run " << aRun->GetRunID() << " start." << G4endl;
+
+  // Get the analysis manager
+  auto analysisManager = G4AnalysisManager::Instance();
+  G4cout << "Using " << analysisManager->GetType() << G4endl;
   
+  // Get time and date to creating a unique filename
+  time_t rawtime;
+  struct tm * timeinfo;
+  char buffer[80];
+  
+  time (&rawtime);
+  timeinfo = localtime(&rawtime);
+  
+  strftime(buffer,80,"-%d-%m-%Y-%H-%M-%S",timeinfo);
+  G4String DateTime(buffer);
+  
+  // Open an output file
+  G4String fileName = "pINT_output" + DateTime + ".root";  
+  //  G4String fileName = "pINT_output.root";
+  analysisManager->OpenFile(fileName);
     
   //inform the runManager to save random number seed
   G4RunManager::GetRunManager()->SetRandomNumberStore(true);
